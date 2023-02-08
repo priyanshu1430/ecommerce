41d7475c388d58d218b43336235148994875d631
priyanshu1430 <priyanshugpt143@gmail.com>
2023-02-08 16:02:25 +0530

    half

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import ProductList from "./pages/ProductList";
 import Home from "./pages/Home";
+import Product from "./pages/Product";
 
 const App = ()=>{
-    return <Home/>
+    return <Product/>
 };
 
 export default App;
